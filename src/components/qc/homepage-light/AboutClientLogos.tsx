9cf22fd01dc8abd6f44856c61523dda55c6d8d7a
satabdimohanty2000 <satabdimohanty38@gmail.com>
2026-06-04 20:57:29 +0530

    style: change AboutClientLogos wrapper to max-w-7xl

--- src/components/qc/homepage-light/AboutClientLogos.tsx
+++ src/components/qc/homepage-light/AboutClientLogos.tsx
@@ -290,7 +290,7 @@ export default function AboutClientLogos({
       {/* Top clean hairline edge */}
       <div className="absolute left-0 right-0 top-0 h-px bg-neutral-900/[0.06]" />
 
-      <div className="relative mx-auto max-w-[1400px] px-6 lg:px-12">
+      <div className="relative mx-auto max-w-7xl px-6 lg:px-12">
         
         {!marqueeOnly && (
         <motion.div
